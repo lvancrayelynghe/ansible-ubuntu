60befb76620f4f8d257264c22cc823cf96f4ee27
Benoth <benoth83@gmail.com>
2016-01-26 13:46:02 +0100

    Add basic Ansible install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###################
+# Install ansible #
+if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    echo "Adding Ansible PPA"
+    sudo apt-add-repository ppa:ansible/ansible -y
+fi
+
+if ! hash ansible >/dev/null 2>&1; then
+    echo "Installing Ansible..."
+    sudo apt-get update
+    sudo apt-get install software-properties-common ansible git python-apt -y
+else
+    echo "Ansible already installed"
+fi
+
