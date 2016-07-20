aa10b1576fcfe49f9f22f1645fa577e0697beebe
Luc Vancrayelynghe <benoth83@gmail.com>
2016-07-20 13:22:19 +0200

    Update installation script to display install process

--- install.sh
+++ install.sh
@@ -15,7 +15,9 @@ else
     echo "Ansible already installed"
 fi
 
-###################
-# Install ansible #
-echo "Run ansible with :"
-echo "  ansible-playbook ansible-desktop.yml"
+#####################################
+# Display real installation process #
+echo ""
+echo "Customize the playbook ansible-desktop.yml to suit your needs, then run ansible with :"
+echo "  ansible-playbook ansible-desktop.yml --ask-become-pass"
+echo ""
